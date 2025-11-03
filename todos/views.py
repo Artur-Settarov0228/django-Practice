a24f98955c53828b5c2f44552478f0786169384a
settarovartur <settarovartur2@gamil.com>
2025-11-04 01:34:05 +0500

    init

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from uuid import uuid4
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+task_list = []
+
+
+def get_task_list_view(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name='list.html', context={'tasks': task_list})
+
+
+def add_task_view(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return render(request=request, template_name='add.html')
+
+    elif request.method == 'POST':
+        form = request.POST
+
+        title = form.get('title')
+        description = form.get('description')
+        priority = form.get('priority')
+        status = form.get('status')
+        deadline = form.get('deadline')
+
+        # validation
+        if not title:
+            return HttpResponse("Titleni unitdingiz.", status=400)
+        elif priority not in ['low', 'medium', 'high']:
+            return HttpResponse("Priorityni noto‘g‘ri kiritdingiz.", status=400)
+        elif status not in ['todo', 'inprogress', 'done']:
+            return HttpResponse("Statusni noto‘g‘ri kiritdingiz.", status=400)
+        elif not deadline:
+            return HttpResponse("Deadlineni unitdingiz.", status=400)
+        else:
+            new_task = {
+                'id': str(uuid4()),
+                'title': title,
+                'description': description,
+                'priority': priority,
+                'status': status,
+                'deadline': deadline,
+            }
+            task_list.append(new_task)
+            return redirect(reverse('todos:list'))
+
+
+def task_detail_view(request: HttpRequest, task_id: str) -> HttpResponse:
+    for task in task_list:
+        if task['id'] == task_id:
+            return render(request=request, template_name='detail.html', context={'task': task})
+    return redirect(reverse('todos:list'))
